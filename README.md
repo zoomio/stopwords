b943f89a93d5378dacbfcc7fa34bfc14219cabc6
smeshkov <smeshkow@gmail.com>
2018-08-06 15:43:00 +1000

    first release, and it will have stop-words registered.

---
+++ README.md
@@ -0,0 +1,20 @@
+# Stop Words
+List of stop-words for convenient consumption in Go projects.
+
+## Usage ##
+
+* import `github.com/zoomio/stopwords` and it will have stop-words registered;
+* use `stopwords#IsStopWord` to check whether given string is a stop-word;
+* use `stopwords#Slice` to retriev a list of registered stop-words.
+
+## Changelog
+
+See [CHANGELOG.md](https://raw.githubusercontent.com/zoomio/stopwords/master/CHANGELOG.md)
+
+## Contributing
+
+See [CONTRIBUTING.md](https://raw.githubusercontent.com/zoomio/stopwords/master/CONTRIBUTING.md)
+
+## License
+
+Released under the [Apache License 2.0](https://raw.githubusercontent.com/zoomio/stopwords/master/LICENSE).
---
+++ register.go
@@ -0,0 +1,33 @@
+package stopwords
+
+import (
+	"strings"
+)
+
+var (
+	// StopWordsIndex - vocabulary of stop-words for quick search on contains
+	stopWordsIndex = make(map[string]bool)
+	stopWords = make([]string, 0)
+)
+
+func init()  {
+	RegisterStopWords(strings.Split(StopWords, "\n"))
+}
+
+// RegisterStopWords registers given list of stop-words to use as vocabulary.
+func RegisterStopWords(words []string) {
+	stopWords = append(stopWords, words...)
+	for _, s := range words {
+		stopWordsIndex[s] = true
+	}
+}
+
+// IsStopWord returns true if given string as a stop-word.
+func IsStopWord(s string) bool {
+	return stopWordsIndex[s]
+}
+
+// Slice returns list of all registered stop-words.
+func Slice() []string {
+	return append([]string(nil), stopWords...)
+}
